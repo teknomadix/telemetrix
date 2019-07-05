c3afb4c2a9f1e109e2568171897098aa2dd29395
Luke Nimtz <luke.nimtz@gmail.com>
2019-07-05 13:22:07 -0700

    basic reading of gpsd lat lon

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Make sure `gpsd` is running with a detected device.
+
+    cargo run
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use unbounded_gpsd::{types::*, GpsdConnection};
+
+fn main() {
+    println!("Hello, world!");
+    let mut gpsd_con = GpsdConnection::new("localhost:2947").expect("connection failed");
+    gpsd_con.watch(true).expect("watch failed");
+    loop {
+        println!("getting res...");
+        let res = gpsd_con.get_response().expect("get res failed");
+        match res {
+            Response::Tpv(tpv) => match tpv {
+                TpvResponse::Fix3D { lat, lon, .. } => {
+                    println!("{}, {}", lat, lon);
+                }
+                _ => {
+                    println!("some other fix");
+                }
+            },
+            _ => {
+                println!("some other response");
+            }
+        }
+    }
+}
