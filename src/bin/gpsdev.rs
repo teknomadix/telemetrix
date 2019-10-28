497f37a1d6a752186ecab21504b9c6265e70fb9d
Luke Nimtz <luke.nimtz@gmail.com>
2019-10-27 18:10:30 -0700

    switching to udev

--- src/bin/gpsdev.rs
+++ src/bin/gpsdev.rs
@@ -1,30 +1,13 @@
-use libc::types::os::arch::c95::{c_int, size_t};
-use std::ptr;
-
-use usb::libusb::{
-    libusb_device, libusb_exit, libusb_free_device_list, libusb_get_device_list, libusb_init,
-};
+extern crate udev;
 
 fn main() {
     println!("Hello, world!");
-    // unsafe calls to libusb
-    unsafe {
-        // use a null context (will use a default context)
-        // let null_context: *mut *mut libusb_context = ptr::null_mut();
-        let r: c_int = libusb_init(ptr::null_mut());
-        if r < 0 {
-            std::process::exit(r);
-        }
+    let context = udev::Context::new().unwrap();
+    let mut enumerator = udev::Enumerator::new(&context).unwrap();
 
-        let mut devs: *mut *mut libusb_device = ptr::null_mut();
-        let cnt: size_t = libusb_get_device_list(ptr::null_mut(), &mut devs);
-        if cnt < 0 {
-            libusb_exit(ptr::null_mut());
-            std::process::exit(1);
-        }
-        println!("{:?}", devs);
-        libusb_free_device_list(devs, 1);
+    enumerator.match_subsystem("tty").unwrap();
 
-        libusb_exit(ptr::null_mut());
+    for device in enumerator.scan_devices().unwrap() {
+        println!("found device: {:?}", device.syspath());
     }
 }
