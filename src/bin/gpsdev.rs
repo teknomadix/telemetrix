a62da610710cbc04903dd73e7a98467dd40ee394
Luke Nimtz <luke@teknomadix.com>
2019-10-29 04:35:01 +0000

    functional error handling... i think

--- src/bin/gpsdev.rs
+++ src/bin/gpsdev.rs
@@ -1,20 +1,25 @@
 extern crate udev;
 
-fn main() {
+fn main() -> Result<(), udev::Error> {
     println!("Hello, world!");
-    let context = udev::Context::new().unwrap();
-    let mut enumerator = udev::Enumerator::new(&context).unwrap();
-
-    enumerator.match_subsystem("tty").unwrap();
-
-    for device in enumerator.scan_devices().unwrap() {
-        println!("found device: {:?}", device.syspath());
-        println!("parent: {:?}", device.parent().unwrap().sysname());
-        for property in device.properties() {
-            println!("{:?} = {:?}", property.name(), property.value());
-        }
-        for attribute in device.attributes() {
-            println!("{:?} = {:?}", attribute.name(), attribute.value());
-        }
-    }
+    udev::Context::new()
+        .and_then(|context| udev::Enumerator::new(&context))
+        .and_then(|mut enumerator| {
+            enumerator
+                .match_subsystem("tty")
+                .and(enumerator.scan_devices())
+        })
+        .and_then(|devices| {
+            devices.for_each(|device| {
+                println!("found device: {:?}", device.syspath());
+                // println!("parent: {:?}", device.parent().unwrap().sysname());
+                device.properties().for_each(|property| {
+                    println!("{:?} = {:?}", property.name(), property.value());
+                });
+                device.attributes().for_each(|attribute| {
+                    println!("{:?} = {:?}", attribute.name(), attribute.value());
+                });
+            });
+            Ok(())
+        })
 }
