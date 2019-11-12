9d5b20a96de2d91d2b1839ff98e42656f0755447
Luke Nimtz <luke.nimtz@gmail.com>
2019-11-11 21:19:51 -0800

    making gpsd socket command

--- src/bin/gpsdev.rs
+++ src/bin/gpsdev.rs
@@ -1,7 +1,12 @@
+extern crate tokio;
 extern crate udev;
 
+use std::error::Error;
+use tokio::fs;
+use tokio::prelude::*;
+
 // TODO: use `failure` crate for error interop
-fn main() -> Result<(), udev::Error> {
+fn main() -> Result<(), Box<Error>> {
     println!("Hello, world!");
     let context = udev::Context::new()?;
     let mut enumerator = udev::Enumerator::new(&context)?;
@@ -21,13 +26,19 @@ fn main() -> Result<(), udev::Error> {
                 .is_some()
     });
 
+    // TODO warnings for failures getting device name?
     for device in gps_devices {
-        let dev_name = device
+        device
             .properties()
             .find(|prop| prop.name() == "DEVNAME")
-            .map(|prop| prop.value().to_os_string());
-
-        println!("GPS found device: {:?}", dev_name);
+            .and_then(|prop| prop.value().to_os_string().into_string().ok())
+            .map(|dev_name| {
+                println!("GPS found device: {:?}", dev_name);
+                let mut cmd = String::from("+");
+                cmd.push_str(&dev_name[..]);
+                println!("GPS command: {:?}", cmd);
+                // TODO: Tokio write to unix socket
+            });
     }
 
     Ok(())
